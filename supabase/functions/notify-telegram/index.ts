4183b96abb02a1aa6b2169864487351847de0432
Alvaro <alvarorey7@gmail.com>
2025-11-26 23:08:29 +0100

    Add telegram FILES & NWE FUNCv2

--- supabase/functions/notify-telegram/index.ts
+++ supabase/functions/notify-telegram/index.ts
@@ -2,7 +2,7 @@ import { serve } from 'https://deno.land/std@0.219.0/http/server.ts';
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.44.2'; 
 
 const BOT_TOKEN = Deno.env.get("TELEGRAM_BOT_TOKEN")!;
-const SUPABASE_URL = Deno.env.get('NEXT_PUBLIC_SUPABASE_URL')!; 
+const SUPABASE_URL = Deno.env.get('SUPABASE_URL')!; 
 const SUPABASE_SERVICE_ROLE_KEY = Deno.env.get('SUPABASE_SERVICE_ROLE_KEY')!; 
 
 const TELEGRAM_API_URL = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`;
